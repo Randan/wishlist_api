de0e4677d1c2abc7a62d65d41bd2ca39d4067b2d
Andrew Rudai <andrewrandan@gmail.com>
2021-07-06 18:35:01 +0300

    database connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      { useFindAndModify: false },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
